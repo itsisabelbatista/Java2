d4775cee04b96ed1fec6b55f7345414f9f85e375
Isabel Batista <45838155+itsisabelbatista@users.noreply.github.com>
2024-02-15 12:53:56 -0500

    Delete main directory

--- main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module calorie.calculator.calcalculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens calorie.calculator.calcalculator to javafx.fxml;
-    exports calorie.calculator.calcalculator;
-}
